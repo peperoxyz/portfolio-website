3c4183ce53bde847d7197fb3da97aca91fd5504e
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-08 15:40:09 +0700

    Change background color to primary-dark

--- src/app/ui/components/header.tsx
+++ src/app/ui/components/header.tsx
@@ -20,7 +20,7 @@ export const Header = () => {
 
 	return (
 		<div>
-			<div className={`${isSticky ? "sticky top-0" : ""} bg-primary-darkest text-white transition-all duration-300 ease-in-out`}>
+			<div className={`${isSticky ? "sticky top-0" : ""} bg-primary-dark text-white transition-all duration-300 ease-in-out`}>
 				<div className="bg-gradient-to-r from-violet-200 to-violet-700 h-0.5 md:h-1"></div>
 				<div className="container mx-auto px-6 py-4 mt-5 flex justify-center md:justify-between items-center">
 					<div className="text-2xl hidden md:block">{"</>"}</div>
@@ -30,8 +30,8 @@ export const Header = () => {
 								<Link
 									key={item}
 									href={item === "Home" ? "/" : `/${item.toLowerCase()}`}
-									className={`hover:bg-primary-darkest hover:rounded-full hover:px-5 hover:py-2 transition-all px-4 duration-300  ${
-										pathname === (item === "Home" ? "/" : `/${item.toLowerCase()}`) ? "text-violet-400 px-5 py-2 rounded-full bg-primary-darkest" : "text-white"
+									className={`hover:bg-primary-dark hover:rounded-full hover:px-5 hover:py-2 transition-all px-4 duration-300  ${
+										pathname === (item === "Home" ? "/" : `/${item.toLowerCase()}`) ? "text-violet-400 px-5 py-2 rounded-full bg-primary-dark" : "text-white"
 									}`}
 								>
 									{item}
@@ -42,7 +42,7 @@ export const Header = () => {
 					<div className="text-2xl font-bold"></div>
 				</div>
 			</div>
-			<div className="h-[30rem] bg-primary-darkest"></div>
+			{/* <div className="h-[30rem] bg-primary-dark"></div> */}
 		</div>
 	);
 };

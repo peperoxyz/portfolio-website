2a39272e6e03fe4a57dcef7f9af070fba99d691a
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-10 06:31:50 +0700

    Create component for social links icon and badge

--- src/app/ui/components/social-icons.tsx
+++ src/app/ui/components/social-icons.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import githubIcon from "/src/public/images/github.png";
 import twitterIcon from "/src/public/images/twitter.png";
 import linkedinIcon from "/src/public/images/linkedin.png";
+import liveSiteIcon from "/src/public/images/live-site.png";
 
-// Reusable component for each social icon link
 interface SocialIconLinkProps {
 	href: string;
 	iconSrc: StaticImageData;
@@ -19,12 +19,21 @@ const SocialIconLink: React.FC<SocialIconLinkProps> = ({ href, iconSrc, altText 
 	</Link>
 );
 
-export const SocialIcons = ({ links }: { links: { github: string; twitter: string; linkedin: string } }) => {
-	return (
-		<div className="bg-primary-darkest border border-violet-dark rounded-full px-2 py-2 text-sm flex gap-2">
-			<SocialIconLink href={links.github} iconSrc={githubIcon} altText="Github-icon" />
-			<SocialIconLink href={links.twitter} iconSrc={twitterIcon} altText="Twitter-icon" />
-			<SocialIconLink href={links.linkedin} iconSrc={linkedinIcon} altText="Linkedin-icon" />
-		</div>
-	);
+export const SocialIcons = ({ links }: { links: { github: string; twitter: string; linkedin: string; liveSite: string; tipe: string } }) => {
+	if (links.tipe === "socials") {
+		return (
+			<div className="bg-primary-darkest border border-violet-dark rounded-full px-2 py-2 text-sm flex gap-2">
+				<SocialIconLink href={links.github} iconSrc={githubIcon} altText="Github-icon" />
+				<SocialIconLink href={links.twitter} iconSrc={twitterIcon} altText="Twitter-icon" />
+				<SocialIconLink href={links.linkedin} iconSrc={linkedinIcon} altText="Linkedin-icon" />
+			</div>
+		);
+	} else {
+		return (
+			<div className="bg-primary-darkest border border-violet-dark rounded-full px-2 py-2 text-sm flex gap-2">
+				<SocialIconLink href={links.github} iconSrc={githubIcon} altText="Github-icon" />
+				<SocialIconLink href={links.liveSite} iconSrc={liveSiteIcon} altText="LiveSite-icon" />
+			</div>
+		);
+	}
 };

5bed9684dc97f1e6903125c13255c6517acca80f
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-09 13:26:08 +0700

    Create social icons component

---
+++ src/app/ui/components/social-icons.tsx
@@ -0,0 +1,30 @@
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import githubIcon from "/src/public/images/github.png";
+import twitterIcon from "/src/public/images/twitter.png";
+import linkedinIcon from "/src/public/images/linkedin.png";
+
+// Reusable component for each social icon link
+interface SocialIconLinkProps {
+	href: string;
+	iconSrc: StaticImageData;
+	altText: string;
+}
+
+const SocialIconLink: React.FC<SocialIconLinkProps> = ({ href, iconSrc, altText }) => (
+	<Link className="hover:scale-105 transition-all duration-100" href={href}>
+		<div className="h-10 w-10 flex justify-center items-center bg-violet-dark rounded-full border border-slate-800/50">
+			<Image alt={altText} className="w-5" src={iconSrc} />
+		</div>
+	</Link>
+);
+
+export const SocialIcons = ({ links }: { links: { github: string; twitter: string; linkedin: string } }) => {
+	return (
+		<div className="bg-primary-darkest border border-violet-dark rounded-full px-2 py-2 text-sm flex gap-2">
+			<SocialIconLink href={links.github} iconSrc={githubIcon} altText="Github-icon" />
+			<SocialIconLink href={links.twitter} iconSrc={twitterIcon} altText="Twitter-icon" />
+			<SocialIconLink href={links.linkedin} iconSrc={linkedinIcon} altText="Linkedin-icon" />
+		</div>
+	);
+};

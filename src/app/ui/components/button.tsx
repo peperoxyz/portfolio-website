046889e68d64562aa7397828bc5e5f4ba85facd6
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-09 12:18:38 +0700

    Add new param for the button's dynamic name

--- src/app/ui/components/button.tsx
+++ src/app/ui/components/button.tsx
@@ -1,14 +1,14 @@
 import { clsx } from "clsx";
+import { ArrowRight } from "react-feather";
 
-export const ButtonViolet = (params: any) => {
-	const { name } = params;
-	const { color } = params;
+export const Button = (params: any) => {
+	const { name, color, button } = params;
 
 	return (
 		<button
 			className={clsx(
 				color ? "bg-violet-dark border border-slate-700/20 shadow-inner hover:bg-primary-dark hover:border hover:border-violet-dark" : "bg-primary-dark border border-violet-dark hover:bg-violet-dark",
-				"px-8 py-[0.8rem] text-sm md:text-base rounded-lg hover:text-white transition-all duration-300 ease-in-out"
+				"px-8 py-[0.8rem] text-sm md:text-base rounded-lg hover:text-white transition-all duration-300 ease-in-out text-gray-light"
 			)}
 		>
 			{name}
--- src/app/ui/components/divider.tsx
+++ src/app/ui/components/divider.tsx
@@ -1,7 +1,7 @@
 import { Button } from "./button";
 
 export const Divider = (params: any) => {
-	const { title, name } = params.sectionDetails;
+	const { title, name, button } = params.sectionDetails;
 	console.log(params);
 
 	return (
@@ -9,21 +9,12 @@ export const Divider = (params: any) => {
 			<div className="container max-w-lg sm:max-w-[30rem] md:max-w-3xl lg:max-w-[62rem] xl:max-w-[77rem] flex justify-between border mt-16 text-white uppercase pb-6 border-slate-300/30 border-t-0 border-l-0 border-r-0">
 				<div>
 					<span className="text-gray-light text-xs md:text-sm">{title}</span>
-					<h3 className="text-lg md:text-2xl">{name}</h3>
+					<h3 className="text-lg md:text-3xl">{name}</h3>
 				</div>
 				<div>
-					<Button color={true} name={"Contact me"} arrowRightIcon={true} />
+					<Button color={true} name={button} arrowRightIcon={true} />
 				</div>
 			</div>
 		</div>
-		// <div className="flex justify-center">
-		// 	<div className="container py-4 mt-20 relative text-white border">
-		// 		<div className="flex justify-between">
-		// 			<div>1</div>
-		// 			<div>2</div>
-		// 		</div>
-		// 	</div>
-		// 	{/* <div className="container mx-auto w-[92%] border">haha</div> */}
-		// </div>
 	);
 };

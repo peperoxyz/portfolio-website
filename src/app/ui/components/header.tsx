355d4e4a004c904f5e660dcffe3c969129ba0083
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-09 12:20:10 +0700

    Adjust spacing in header

--- src/app/ui/components/header.tsx
+++ src/app/ui/components/header.tsx
@@ -21,7 +21,7 @@ export const Header = () => {
 	return (
 		<div className={`${isSticky ? "sticky top-0 z-50" : ""} bg-primary-dark text-white transition-all duration-300 ease-in-out`}>
 			<div className="bg-gradient-to-r from-violet-200 to-violet-700 h-0.5 md:h-1"></div>
-			<div className="container py-5 mx-auto px-6 lg:px-20 flex justify-center md:justify-between items-center">
+			<div className="container py-5 mx-auto max-w-lg sm:max-w-[30rem] md:max-w-3xl lg:max-w-[62rem] xl:max-w-[77rem] flex justify-center md:justify-between items-center">
 				<div className="text-2xl hidden md:block">{"</>"}</div>
 				<div className="md:-ml-10 border-violet-dark bg-violet-dark text-xs md:text-base rounded-full w-fit border-slate-700/20 shadow-inner shadow-black/50 py-[0.1rem] ">
 					<nav className="flex gap-[0.3rem] px-[0.3rem] md:px-2 py-0.5 md:pt-1 md:pb-[0.125rem] items-center">

5026af4b2b568d1fc851517338294583bdc7998e
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-10 11:32:31 +0700

    Create page for about me section

---
+++ src/app/(pages)/about/page.jsx
@@ -0,0 +1,11 @@
+import { Header } from "@/app/ui/components/header";
+import { AboutMe } from "@/app/ui/components/about-me";
+
+export default function Home() {
+	return (
+		<div>
+			<Header></Header>
+			<AboutMe />
+		</div>
+	);
+}

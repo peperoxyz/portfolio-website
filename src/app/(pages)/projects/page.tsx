41940b4e9cb3f5fef5c6305e58bbe42eee6d67c1
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-10 11:32:45 +0700

    Add project wrapper component

--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/app/ui/components/header";
+import { ProjectWrapper } from "@/app/ui/components/projects-wrapper";
 
 export default function Home() {
 	return (
 		<div>
 			<Header></Header>
+			<ProjectWrapper />
 		</div>
 	);
 }

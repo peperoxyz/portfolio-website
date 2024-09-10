dd28c1b5c5a8a63243999b571ca282eb08067ed6
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-10 10:07:25 +0700

    Create page for projects

---
+++ src/app/(pages)/projects/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/app/ui/components/header";
+import ProjectCard from "@/app/ui/components/project-card";
+import angularIcon from "/src/public/images/angular-icon.png";
+import tailwindIcon from "/src/public/images/tailwind-icon.png";
+
+export default function Home() {
+	return (
+		<div>
+			<Header></Header>
+		</div>
+	);
+}

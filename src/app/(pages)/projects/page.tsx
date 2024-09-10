99cb655f4ef3f435543693310d0bc84da933e7fe
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-10 10:15:49 +0700

    Remove unused imports

--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -1,7 +1,4 @@
 import { Header } from "@/app/ui/components/header";
-import ProjectCard from "@/app/ui/components/project-card";
-import angularIcon from "/src/public/images/angular-icon.png";
-import tailwindIcon from "/src/public/images/tailwind-icon.png";
 
 export default function Home() {
 	return (
